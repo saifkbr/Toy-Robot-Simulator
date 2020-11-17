dfb26b014a745dfb9a0bcb95e9e8526f4161e632
saifkbr <saif_kbr@yahoo.co.in>
2020-11-17 22:44:02 +0000

    Empty projects added to the solution

---
+++ Toy-Robot-Simulator/RobotSimulatorConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RobotSimulatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
