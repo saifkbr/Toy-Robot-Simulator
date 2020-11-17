04da08d3b7f6f6e43f1e4d6ae7155251da6a9890
saifkbr <saif_kbr@yahoo.co.in>
2020-11-17 23:28:38 +0000

    Creation of TableTop implemented

---
+++ Toy-Robot-Simulator/RobotSimulatorService/TableTop.cs
@@ -0,0 +1,29 @@
+﻿namespace RobotSimulatorService
+{
+    using System.Drawing;
+
+    public interface ITableTop
+    {
+        Point[,] DrawTableTop();
+    }
+
+    public class TableTop : ITableTop
+    {
+        public Point[,] DrawTableTop()
+        {
+            int row = 5, column = 5;
+            var points = new Point[row, column];
+            int maxRow = row - 1, maxCol = column - 1;
+
+            for (int i = maxRow; i >= 0; i--)
+            {
+                for (int j = maxCol; j >= 0; j--)
+                {
+                    points[i, j] = new Point { X = maxCol - j, Y = maxRow - i };
+                }
+            }
+
+            return points;
+        }
+    }
+}
