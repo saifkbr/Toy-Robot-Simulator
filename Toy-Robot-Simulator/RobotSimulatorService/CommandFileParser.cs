b637488a1e91a940818b995d7c68ffc4a0f1bee4
saifkbr <saif_kbr@yahoo.co.in>
2020-11-18 04:27:01 +0000

    Console app added to play with the simulator service.

--- Toy-Robot-Simulator/RobotSimulatorService/CommandFileParser.cs
+++ Toy-Robot-Simulator/RobotSimulatorService/CommandFileParser.cs
@@ -19,7 +19,11 @@
                 setOfCompleteCommands.Contains(Command.PLACE.ToString()) &&
                 setOfCompleteCommands.Contains(Command.REPORT.ToString()))
             {
-                commands = setOfCompleteCommands.Split(Command.REPORT.ToString(), StringSplitOptions.RemoveEmptyEntries).ToList();
+                var commandSet = setOfCompleteCommands.Split(Command.REPORT.ToString(), StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < commandSet.Length; i++)
+                {
+                    commands.Add(commandSet[i] += $"{Command.REPORT.ToString()}");
+                }
             }
 
             return commands;
