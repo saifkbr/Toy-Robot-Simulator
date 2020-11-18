8a7f835bb5010edf1d3522e917b0361690b5b520
saifkbr <saif_kbr@yahoo.co.in>
2020-11-18 01:11:44 +0000

    Command file parser implemented.

---
+++ Toy-Robot-Simulator/RobotSimulatorService/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace RobotSimulatorService
+{
+    public enum Command
+    {
+        PLACE = 0,
+        MOVE = 1,
+        LEFT = 2,
+        RIGHT = 3,
+        REPORT = 4
+    }
+
+    public enum Orientation
+    {
+        NORTH = 0,
+        SOUTH = 1,
+        EAST = 2,
+        WEST = 3
+    }
+}
---
+++ Toy-Robot-Simulator/RobotSimulatorTest/CommandFileParserTest.cs
@@ -0,0 +1,25 @@
+﻿namespace RobotSimulatorTest
+{
+    using RobotSimulatorService;
+    using System.Reflection;
+    using Xunit;
+
+    public class CommandFileParserTest
+    {
+        [Theory]
+        [InlineData(@"Resources.Commands1.txt", 3)]
+        [InlineData(@"Resources.Commands2.txt", 1)]
+        [InlineData(@"Resources.Commands3.txt", 0)]
+        [InlineData(@"Resources.Commands4.txt", 1)]
+        public async System.Threading.Tasks.Task ProcessReadAsync_ShouldReturn_ListOfCommandsAsync(string filename, int setOfIndividualCommands)
+        {
+            ICommandFileParser parser = new CommandFileParser();
+
+            var setOfCompleteCommands = await Assembly.GetExecutingAssembly().GetEmbeddedResourceStreamAsync(filename);
+
+            var commandsList = parser.GetCommandsList(setOfCompleteCommands);
+
+            Assert.Equal(setOfIndividualCommands, commandsList.Count);
+        }
+    }
+}
